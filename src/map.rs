4453150acf33c5658e75380565c1a8e3b1e1fda0
Piletskii-Oleg <piletskii.oleg@gmail.com>
2024-05-22 16:24:48 +0300

    make scrub work with hashmap

--- src/map.rs
+++ src/map.rs
@@ -1,17 +1,19 @@
-use std::collections::HashMap;
+use std::fmt::Formatter;
 use std::io;
 use std::time::Duration;
 
 use crate::{ChunkHash};
 
+#[derive(Clone, Debug)]
+struct DataStruct<K>(Data<K>);
+
 #[derive(Clone)]
-pub enum Data {
+pub enum Data<K> {
     Chunk(Vec<u8>),
-    TargetChunk,
+    TargetChunk(Vec<K>),
 }
 
-pub type CDCMap<Hash> = Box<dyn Map<Hash, Data>>;
-pub type ChunkMap<Hash> = Box<dyn Map<Hash, Vec<u8>>>;
+pub type ChunkMap<K> = Box<dyn Map<K, Vec<u8>>>;
 
 pub trait Map<K, V> {
     fn insert(&mut self, key: K, value: V) -> io::Result<()>;
@@ -39,40 +41,41 @@ pub struct ScrubMeasurements {
     data_left: usize,
 }
 
-pub trait Scrub<Hash: ChunkHash> {
-
-    fn scrub(&mut self, cdc_map: &mut dyn Iterator<Item = (&Hash, &mut Data)>, target_map: &mut ChunkMap<Hash>) -> ScrubMeasurements;
+pub trait Scrub<Hash: ChunkHash, K> {
+    fn scrub(&mut self, cdc_map: &mut dyn Iterator<Item = (&Hash, &mut DataStruct<K>)>, target_map: &mut ChunkMap<K>) -> ScrubMeasurements;
 }
 
-enum MapType {
-    Cdc,
-    Target
-}
-
-pub struct ChunkStorage<Hash: ChunkHash, CDC: Map<Hash, Data>> {
+pub struct ChunkStorage<Hash: ChunkHash, CDC: Map<Hash, DataStruct<K>>, K> {
     cdc_map: CDC,
-    scrubber: Box<dyn Scrub<Hash>>,
-    target_map: ChunkMap<Hash>,
-    correspondence_map: HashMap<Hash, MapType>
+    scrubber: Box<dyn Scrub<Hash, K>>,
+    target_map: ChunkMap<K>,
 }
 
-impl<Hash: ChunkHash, CDC> ChunkStorage<Hash, CDC>
+impl<Hash: ChunkHash, CDC, K> ChunkStorage<Hash, CDC, K>
 where
-    CDC: Map<Hash, Data>
+    CDC: Map<Hash, DataStruct<K>>
 {
-    pub fn new(cdc_map: CDC, target_map: ChunkMap<Hash>, scrubber: Box<dyn Scrub<Hash>>) -> Self {
+    pub fn new(cdc_map: CDC, target_map: ChunkMap<K>, scrubber: Box<dyn Scrub<Hash, K>>) -> Self {
         Self {
             cdc_map,
             scrubber,
             target_map,
-            correspondence_map: HashMap::default(),
         }
     }
 }
 
-impl Default for Data {
-    fn default() -> Self {
-        Data::Chunk(vec![])
+impl<K> DataStruct<K> {
+    fn make_target(&mut self, keys: Vec<K>) {
+        self.0 = Data::TargetChunk(keys);
+    }
+}
+
+impl<K> std::fmt::Debug for Data<K> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Data::Chunk(_) => write!(f, "Chunk"),
+            Data::TargetChunk(_) => write!(f, "TargetChunk")
+        }
     }
 }
 
@@ -81,29 +84,34 @@ mod tests {
     use std::collections::HashMap;
     use crate::base::HashMapBase;
     use crate::ChunkHash;
-    use crate::map::{ChunkMap, ChunkStorage, Data, Scrub, ScrubMeasurements};
+    use crate::map::{ChunkMap, ChunkStorage, Data, DataStruct, Scrub, ScrubMeasurements};
 
     struct DumbScrubber;
 
-    impl<Hash: ChunkHash> Scrub<Hash> for DumbScrubber {
-        fn scrub(&mut self, cdc: &mut dyn Iterator<Item = (&Hash, &mut Data)>, target: &mut ChunkMap<Hash>) -> ScrubMeasurements {
-            for (hash, chunk) in cdc {
-
+    impl<Hash: ChunkHash, K> Scrub<Hash, K> for DumbScrubber {
+        fn scrub(&mut self, cdc: &mut dyn Iterator<Item = (&Hash, &mut DataStruct<K>)>, target: &mut ChunkMap<K>) -> ScrubMeasurements {
+            for (chunk, data) in cdc {
+                data.make_target(vec![]);
             }
-            ScrubMeasurements::default()
+
+           ScrubMeasurements::default()
         }
     }
 
     #[test]
     fn hashmap_works_as_cdc_map() {
-        let mut chunk_storage: ChunkStorage<i32, _> = ChunkStorage {
-            cdc_map: HashMap::default(),
+        let mut map = HashMap::new();
+        map.insert(1, DataStruct(Data::Chunk(vec![])));
+        map.insert(2, DataStruct(Data::Chunk(vec![])));
+        let mut chunk_storage: ChunkStorage<_, _, String> = ChunkStorage {
+            cdc_map: map,
             scrubber: Box::new(DumbScrubber),
             target_map: Box::new(HashMapBase::default()),
-            correspondence_map: HashMap::new()
         };
 
         let measurements = chunk_storage.scrubber.scrub(&mut Box::new(&mut chunk_storage.cdc_map.iter_mut()), &mut chunk_storage.target_map);
-        assert_eq!(measurements, ScrubMeasurements::default())
+        assert_eq!(measurements, ScrubMeasurements::default());
+
+        println!("{:?}", chunk_storage.cdc_map)
     }
 }
