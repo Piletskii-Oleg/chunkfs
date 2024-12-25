60a348e353143ca2f75b7795bc46d258573f38ba
Oleg Piletskiy <o.piletskiy@yadro.com>
2024-12-25 17:49:26 +0300

    unwrap_chunk method in DataContainer

--- src/system/storage.rs
+++ src/system/storage.rs
@@ -336,6 +336,18 @@ impl<K> DataContainer<K> {
     pub fn extract_mut(&mut self) -> &mut Data<K> {
         &mut self.0
     }
+
+    /// Returns a contained chunk if it is of type `Data::Chunk`.
+    ///
+    /// Will panic otherwise.
+    pub fn unwrap_chunk(&self) -> &Vec<u8> {
+        match &self.0 {
+            Data::Chunk(chunk) => chunk,
+            Data::TargetChunk(_) => {
+                panic!("Target chunk found in DataContainer; expected simple chunk")
+            }
+        }
+    }
 }
 
 impl<K> From<Vec<u8>> for DataContainer<K> {
