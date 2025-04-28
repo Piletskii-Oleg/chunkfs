4ddda4a2ca256bc4f41481031218973b04d549ab
Gleb Nasretdinov <gleb.nasretdinov@proton.me>
2025-04-28 13:02:21 +0300

    optimize DiskDatabase for multiple sequential read/write requests by combining them
    Signed-off-by: Gleb Nasretdinov <gleb.nasretdinov@proton.me>

--- src/system/disk_database.rs
+++ src/system/disk_database.rs
@@ -1,27 +1,44 @@
 use crate::{ChunkHash, Database, IterableDatabase};
+use bincode::error::EncodeError;
 use bincode::{decode_from_slice, encode_to_vec, Decode, Encode};
 use libc::O_DIRECT;
 use std::collections::HashMap;
 use std::fs::{File, OpenOptions};
 use std::io;
-use std::io::{Seek, Write};
 use std::marker::PhantomData;
 use std::os::fd::AsRawFd;
 use std::os::unix::fs::{FileExt, OpenOptionsExt};
 use std::path::Path;
 
-/// Information about the location of the data on the disk.
-#[derive(Clone)]
-struct DataInfo {
-    start_block: u64,
-    /// Serialized data length
-    data_length: u64,
-}
-
 /// Constant for requesting total size of the block device via ioctl
 const BLKGETSIZE64: u64 = 0x80081272;
 /// Constant for requesting size of the block in the block device via ioctl
 const BLKSSZGET: u64 = 0x1268;
+
+/// Information about the location of the data on the disk.
+#[derive(Clone)]
+struct DataInfo {
+    /// Offset of the data on the block device.
+    offset: u64,
+    /// Serialized data length.
+    data_length: u64,
+}
+
+/// Continuous data interval with information about sub-intervals. Offsets of the sub-intervals must be sequential and continuous.
+/// Need for more convenient large aggregated read requests.
+///
+/// `Is not written to disk`, only used when processing read operations.
+///
+/// If device is opened with O_DIRECT flag, offset is a multiple of a block size, since it padded at start and end.
+/// Also, with O_DIRECT it is possible that they may overlap during the processing of a multiple write/read operations.
+struct SuperBlock {
+    /// Actual data of the superblock.
+    data: Vec<u8>,
+    /// Superblock offset. First subinterval can
+    offset: u64,
+    /// Sub-intervals info. Must be sequential and continuous.
+    data_infos: Vec<DataInfo>,
+}
 
 enum InitType {
     /// [`DiskDatabase`] is initialized on a block device.
@@ -47,7 +64,7 @@ where
     /// Block size when initialized on a regular file, set to 512.
     block_size: u64,
     /// Number of occupied blocks.
-    used_blocks: u64,
+    used_size: u64,
     /// Whether the device is opened with the O_DIRECT flag.
     o_direct: bool,
     /// Values data type. Database doesn't actually own them, so this field is necessary.
@@ -81,7 +98,7 @@ where
             database_map,
             total_size,
             block_size: 512,
-            used_blocks: 0,
+            used_size: 0,
             o_direct,
             _data_type: PhantomData,
         })
@@ -156,7 +173,7 @@ where
             database_map,
             total_size,
             block_size,
-            used_blocks: 0,
+            used_size: 0,
             o_direct,
             _data_type: PhantomData {},
         })
@@ -174,43 +191,127 @@ where
         }
     }
 
-    /// Serializes and writes data to disk. Returns `DataInfo` with information about the allocated data.
-    fn write<T: Encode>(&mut self, value: T) -> io::Result<DataInfo> {
-        let mut encoded = encode_to_vec(&value, bincode::config::standard())
-            .map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))?;
-        let data_length = encoded.len() as u64;
-
-        if self.used_blocks * self.block_size + data_length >= self.total_size {
-            return Err(io::Error::from(io::ErrorKind::OutOfMemory));
+    /// Constructs [`SuperBlock`] by vector of sequential and continuous [`DataInfo`].
+    ///
+    /// Padded at start and end, if block device is inited with O_DIRECT flag.
+    fn superblock_from_data_infos(&self, data_infos: Vec<DataInfo>) -> io::Result<SuperBlock> {
+        if data_infos.is_empty() {
+            return Err(io::Error::from(io::ErrorKind::InvalidData));
         }
 
-        let blocks_number = data_length.div_ceil(self.block_size);
-        let padding_size = self.padding_to_multiple_block_size(data_length);
-        encoded.extend(vec![0; padding_size as usize]); // padding for work with O_DIRECT flag
-
-        self.device
-            .seek(io::SeekFrom::Start(self.used_blocks * self.block_size))?;
-        self.device.write_all(&encoded)?;
-
-        let data_info = DataInfo {
-            start_block: self.used_blocks,
-            data_length,
+        let first = data_infos.first().unwrap();
+        let last = data_infos.last().unwrap();
+        let (start_padding, end_padding) = if self.o_direct {
+            (
+                first.offset % self.block_size,
+                self.padding_to_multiple_block_size(last.offset + last.data_length),
+            )
+        } else {
+            (0, 0)
         };
-        self.used_blocks += blocks_number;
-        Ok(data_info)
+        let total_len = last.offset + last.data_length - first.offset + start_padding + end_padding;
+
+        Ok(SuperBlock {
+            data: vec![0; total_len as usize],
+            offset: first.offset - start_padding,
+            data_infos,
+        })
     }
 
-    /// Searches for data by `DataInfo`, returns deserialized data.
-    fn read<T: Decode<()>>(&self, data_info: DataInfo) -> io::Result<T> {
-        let mut data = vec![0u8; data_info.data_length as usize];
-        let padding_size = self.padding_to_multiple_block_size(data.len() as u64);
-        data.extend(vec![0; padding_size as usize]);
+    /// Split [`DataInfo`] vector into continuous intervals ([`SuperBlock`]'s).
+    /// If some of the intervals follow each other by offsets, but don't follow each other in the vector, they are split into different intervals.
+    fn split_to_superblocks(&self, data_infos: Vec<&DataInfo>) -> Vec<SuperBlock> {
+        if data_infos.is_empty() {
+            return vec![];
+        }
 
-        self.device
-            .read_at(&mut data, data_info.start_block * self.block_size)?;
-        let (data, _) = decode_from_slice(&data, bincode::config::standard())
+        let mut sequential_data_infos = vec![vec![data_infos.first().unwrap()]];
+        let mut last_end = data_infos[0].offset + data_infos[0].data_length;
+        for data_info in data_infos[1..].iter() {
+            if data_info.offset == last_end {
+                sequential_data_infos.last_mut().unwrap().push(data_info)
+            } else {
+                sequential_data_infos.push(vec![data_info]);
+            }
+            last_end = data_info.offset + data_info.data_length
+        }
+        sequential_data_infos
+            .into_iter()
+            .map(|seq| {
+                self.superblock_from_data_infos(seq.into_iter().map(|&di| di.clone()).collect())
+            })
+            .collect::<io::Result<Vec<SuperBlock>>>()
+            .unwrap()
+    }
+
+    /// Read into superblocks from the block device based on their offsets.
+    fn fill_superblocks(&self, superblocks: Vec<&mut SuperBlock>) -> io::Result<()> {
+        superblocks
+            .into_iter()
+            .map(|superblock| self.device.read_at(&mut superblock.data, superblock.offset))
+            .collect::<io::Result<Vec<_>>>()?;
+        Ok(())
+    }
+
+    /// Decode each sub-interval of each superblock and concats them into vector of decoded values.
+    fn decode_superblocks<T: Decode<()>>(superblocks: Vec<&SuperBlock>) -> io::Result<Vec<T>> {
+        let mut decoded = vec![];
+        superblocks.iter().try_for_each(|&superblock| {
+            superblock.data_infos.iter().try_for_each(|data_info| {
+                let start = (data_info.offset - superblock.offset) as usize;
+                let end = start + data_info.data_length as usize;
+                let (value, _) =
+                    decode_from_slice(&superblock.data[start..end], bincode::config::standard())
+                        .map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))?;
+                decoded.push(value);
+                Ok::<(), io::Error>(())
+            })
+        })?;
+        Ok(decoded)
+    }
+
+    /// Read and decode multiple data from the disk.
+    fn read_multi<T: Decode<()>>(&self, data_infos: Vec<&DataInfo>) -> io::Result<Vec<T>> {
+        if data_infos.is_empty() {
+            return Ok(Vec::new());
+        }
+
+        let mut superblocks = self.split_to_superblocks(data_infos);
+        self.fill_superblocks(superblocks.iter_mut().collect())?;
+        Self::decode_superblocks(superblocks.iter().collect())
+    }
+
+    /// Serializes and writes multiple data to disk. Returns `Vec<DataInfo>` with information about the allocated data.
+    fn write_multi<T: Encode>(&mut self, values: &[&T]) -> io::Result<Vec<DataInfo>> {
+        let encoded = values
+            .iter()
+            .map(|value| encode_to_vec(value, bincode::config::standard()))
+            .collect::<Result<Vec<_>, EncodeError>>()
             .map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))?;
-        Ok(data)
+
+        let encoded_size: usize = encoded.iter().map(|vec| vec.len()).sum();
+        if self.used_size + encoded_size as u64 >= self.total_size {
+            return Err(io::Error::from(io::ErrorKind::OutOfMemory));
+        }
+        let data_infos = encoded.iter().fold(vec![], |mut data_infos, vec| {
+            data_infos.push(DataInfo {
+                offset: self.used_size,
+                data_length: vec.len() as u64,
+            });
+            self.used_size += vec.len() as u64;
+            data_infos
+        });
+
+        let mut encoded = encoded.concat();
+        if self.o_direct {
+            let padding_size = self.padding_to_multiple_block_size(encoded.len() as u64);
+            self.used_size += padding_size;
+            encoded.extend(vec![0; padding_size as usize]); // padding for work with O_DIRECT flag
+        }
+        self.device
+            .write_all_at(&encoded, self.used_size - encoded.len() as u64)?;
+
+        Ok(data_infos)
     }
 }
 
@@ -232,17 +333,38 @@ where
     V: Clone + Encode + Decode<()>,
 {
     fn insert(&mut self, key: K, value: V) -> io::Result<()> {
-        if self.database_map.contains_key(&key) {
-            return Ok(());
-        }
-        let data_info = self.write(value)?;
-        self.database_map.insert(key, data_info);
-        Ok(())
+        self.insert_multi(vec![(key, value)])
     }
 
     fn get(&self, key: &K) -> io::Result<V> {
-        let data_info = self.database_map.get(key).ok_or(io::ErrorKind::NotFound)?;
-        self.read(data_info.clone())
+        self.get_multi(&[key.clone()]).map(|mut vec| vec.remove(0))
+    }
+
+    fn insert_multi(&mut self, pairs: Vec<(K, V)>) -> io::Result<()> {
+        let mut unique_keys = vec![];
+        let mut unique_values = vec![];
+        pairs.iter().for_each(|(k, v)| {
+            if !self.database_map.contains_key(k) {
+                unique_keys.push(k.clone());
+                unique_values.push(v)
+            }
+        });
+        let data_infos = self.write_multi(&unique_values)?;
+        let written_pairs = unique_keys.into_iter().zip(data_infos);
+        self.database_map.extend(written_pairs);
+        Ok(())
+    }
+
+    fn get_multi(&self, keys: &[K]) -> io::Result<Vec<V>> {
+        let data_infos = keys
+            .iter()
+            .map(|k| {
+                self.database_map
+                    .get(k)
+                    .ok_or_else(|| io::Error::new(io::ErrorKind::NotFound, "Key not found"))
+            })
+            .collect::<io::Result<Vec<_>>>()?;
+        self.read_multi(data_infos)
     }
 
     fn contains(&self, key: &K) -> bool {
@@ -284,7 +406,7 @@ where
 
     fn clear(&mut self) -> io::Result<()> {
         self.database_map.clear();
-        self.used_blocks = 0;
+        self.used_size = 0;
         Ok(())
     }
 }
